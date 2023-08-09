97d837282de8a36845d86257e4a7a4cc6b362cb2
vinicius-rosa-magio <viniciusrosamagio@gmail.com>
2023-08-09 09:10:43 -0300

    tarefas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useMemo, useCallback } from "react";
+import './estilo.css';
 
 function App() {
+
+  const [input, setInput] = useState("");
+  const [tarefas, setTarefas] = useState([]);
+
+
+ 
+  useEffect(() => {
+    const tarefasStorage = localStorage.getItem("tarefas");
+    if (tarefasStorage) {
+      setTarefas(JSON.parse(tarefasStorage)); 
+    }
+  }, [])
+
+ 
+  useEffect(() => {
+    localStorage.setItem("tarefas", JSON.stringify(tarefas));
+  }, [tarefas])
+
+
+  const handleAdd = useCallback(() => {
+    setTarefas([...tarefas, input]);
+    setInput("");
+  }, [tarefas, input])
+
+
+  const handleDelete = (index) => {
+    const newTarefas = [...tarefas];
+    newTarefas.splice(index, 1)
+    setTarefas(newTarefas);
+  };
+
+  const totalTarefas = useMemo(() => tarefas.length, [tarefas]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <ul>
+        {tarefas.map((tarefas, index) =>
+          <li key={tarefas} className="listaTarfas">{tarefas}
+            <button onClick={() => handleDelete(index)} className="btnExcluir" >X</button>
+          </li>)}
+
+      </ul>
+      <br />
+
+      <strong>Você tem {totalTarefas} Tarefas!</strong><br />
+
+      <input type="text" value={input} onChange={e => setInput(e.target.value)} className="input" /> {/*Quando queremos alterar o valor do state contador nós usamos o setInput*/}
+      <button type="button" onClick={handleAdd} className="btnAdicionar">Adicionar</button>
+
     </div>
-  );
+  )
 }
 
 export default App;
